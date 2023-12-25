ad6723412592efeb42735ead88c5c8f1b93d1e2c
ariesa4 <tarenadiyaksa@gmail.com>
2023-12-26 00:25:10 +0700

    feat: add include controllers

---
+++ app.js
@@ -0,0 +1,46 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+const fs = require('node:fs')
+
+//include controllers
+fs.readdirSync('controllers').forEach((f) => {
+  if (f.substr(-3) == '.js') {
+    const route = require('./controllers/' + f)
+    route.controller(app)
+  }
+})
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, () => console.log('listening on 3000'))
+
+module.exports = app;
