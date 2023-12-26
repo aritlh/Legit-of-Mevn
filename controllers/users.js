16805992b7a2da210e8edca8cc4fa6bb22afc3e9
ariesa4 <tarenadiyaksa@gmail.com>
2023-12-26 14:19:55 +0700

    feat: testing APIs

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,32 @@
+const User = require('../models/User')
+
 module.exports.controller = (app) => {
     // get users page
     app.get('/users', (req, res) => {
-        res.render('index', {
-            title: "Users",
-            description: "This is the description of all the users"
+        User.find({}, 'name email', (e, users) => {
+            if (e) console.error(e)
+            res.send(users)
+        })
+    })
+
+    // get single user details
+    app.get('/users/:id', (req, res) => {
+        User.findById(req.params.id, 'name email', (e, user) => {
+            if (e) console.error(e)
+            res.send(user)
+        })
+    })
+
+    //add a new user
+    app.post('/users', (req, res) => {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email
+        })
+
+        user.save((e, user) => {
+            if (e) console.error(e)
+            res.send(user)
         })
     })
 }
