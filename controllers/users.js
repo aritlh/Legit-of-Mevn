1ec6b98442c85731735224462d513b0616b45f96
ariesa4 <tarenadiyaksa@gmail.com>
2023-12-26 10:32:38 +0700

    refactor: add description param-object to render method

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
 module.exports.controller = (app) => {
     // get users page
     app.get('/users', (req, res) => {
-        res.render('index', { title: 'Users' })
+        res.render('index', {
+            title: "Users",
+            description: "This is the description of all the users"
+        })
     })
 }
